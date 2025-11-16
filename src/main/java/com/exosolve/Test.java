3ba36effac4d23760e7187286412358a799b02a8
= <=>
2025-11-16 07:58:19 +0545

    test example

---
+++ src/main/java/com/exosolve/Test.java
@@ -0,0 +1,17 @@
+package com.exosolve;
+
+public class Test {
+    public static void main(String[] args) {
+         int one = (int)5.7;
+         double numerator = 5;
+         double denominator = 2;
+        System.out.println((int)(numerator/denominator));
+
+        char a = 'a';
+        int ascii = 38;
+
+        System.out.println((char)ascii);
+
+
+    }
+}
