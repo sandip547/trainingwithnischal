a2a4e0440d2589ecd7f3705793a6589f7b07a435
= <=>
2025-11-26 11:30:46 +0545

    loop exampl

---
+++ src/main/java/com/exosolve/ArrayExample.java
@@ -0,0 +1,17 @@
+package com.exosolve;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int [] asciiValues = new int[5];
+        asciiValues[0] = 'A';
+        asciiValues[1] = 'B';
+        asciiValues[2] = 'C';
+        asciiValues[3] = 'D';
+        asciiValues[4] = 'E';
+
+        // print the decimal value of small letters in the array
+
+
+
+    }
+}
---
+++ src/main/java/com/exosolve/WhileLoopExample.java
@@ -0,0 +1,12 @@
+package com.exosolve;
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        //print number from 5 to 15
+        int count = 2;
+        while (count<=20){
+            System.out.println(count);
+            count +=2;
+        }
+    }
+}
